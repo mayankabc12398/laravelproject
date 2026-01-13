c74b60b309ba089610d835457ac90ff1d377f87e
Mayank K <mayankabc12398@gmail.com>
2026-01-14 00:12:22 +0530

    rename Dockerfile

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
+# Create sqlite file if using sqlite
+mkdir -p /var/www/html/database
+touch /var/www/html/database/database.sqlite
+
 php artisan optimize:clear
 
 php-fpm -D
