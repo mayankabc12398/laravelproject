3c8dada6f8890295a8ccdb57d4a9ce9a0f66b2b4
Mayank K <mayankabc12398@gmail.com>
2026-01-14 10:41:24 +0530

    fix cache driver for render

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ set -e
 mkdir -p /var/www/html/database
 touch /var/www/html/database/database.sqlite
 
-php artisan optimize:clear
-php artisan config:clear
-php artisan cache:clear
+php artisan optimize:clear || true
+php artisan config:clear || true
+php artisan cache:clear || true
 
 php-fpm -D
 nginx -g "daemon off;"
