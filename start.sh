8d32e611c09c5066e4c2dc72c4b354b8ecbe309f
Mayank K <mayankabc12398@gmail.com>
2026-01-14 10:36:03 +0530

    fix cache driver for render

--- start.sh
+++ start.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-# Create sqlite file if using sqlite
 mkdir -p /var/www/html/database
 touch /var/www/html/database/database.sqlite
 
 php artisan optimize:clear
+php artisan config:clear
+php artisan cache:clear
 
 php-fpm -D
 nginx -g "daemon off;"
